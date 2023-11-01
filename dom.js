af4f62b1761911ebd9549597e604496684f7cf7b
py240899 <yadavprakhar04@gmail.com>
2023-11-02 01:05:21 +0530

    get element by class name

--- dom.js
+++ dom.js
@@ -19,6 +19,21 @@ var header = document.getElementById('main-header');
 
 header.style.borderBottom = "solid 3px #000";
 
-var title = document.getElementsByClassName('title')
-console.log(title);
-title.style.color = "green";
+// var title = document.getElementsByClassName('title')
+// console.log(title);
+// title.style.color = "green";
+
+var items = document.getElementsByClassName('list-group-item')
+console.log(items);
+console.log(items[2]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = "bold";
+items[2].style.backgroundColor = "green";
+
+for(var i=0;i<items.length;i++){
+    items[i].style.backgroundColor = "#f4f4f4";
+}
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight ="bold";
+}
